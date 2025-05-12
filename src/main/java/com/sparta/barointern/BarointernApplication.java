afa719b71da4c6c170a96331dca1a58d107a1763
kdh610 <k_dh610@naver.com>
2025-05-12 22:14:54 +0900

    init

---
+++ src/main/java/com/sparta/barointern/BarointernApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.barointern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarointernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarointernApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/barointern/BarointernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.barointern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarointernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
