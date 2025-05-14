48cef68a63df04079401c11c9ebe32781165be7d
kdh610 <k_dh610@naver.com>
2025-05-15 03:19:15 +0900

    refactor: swageer api설명 추가

--- src/main/java/com/sparta/barointern/presentation/UserController.java
+++ src/main/java/com/sparta/barointern/presentation/UserController.java
@@ -6,6 +6,9 @@ import com.sparta.barointern.common.ApiResponse;
 
 import com.sparta.barointern.application.UserService;
 import com.sparta.barointern.application.dto.response.UserAppResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +22,35 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "사용자 관리 API", description = "일반 유저 / 관리자 회원가입 & 관리자권한 부여 & 모든 사용자 정보 조회 기능 ")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "일반 회원 가입", description = "사용자 이름, 닉네임, 비밀번호 입력", tags = {"사용자 관리 API"})
     @PostMapping("/signup")
     public ResponseEntity<UserResponseDto> signup(@RequestBody UserSignupRequestDto userSignupRequest) {
         UserAppResponseDto userAppResponseDto = userService.signup(userSignupRequest.toAppDto());
         return ResponseEntity.ok(UserResponseDto.from(userAppResponseDto));
     }
 
+    @Operation(summary = "관리자 회원 가입", description = "사용자 이름, 닉네임, 비밀번호 입력", tags = {"사용자 관리 API"})
     @PostMapping("/signup/admin")
     public ResponseEntity<UserResponseDto> signupAdmin(@RequestBody UserSignupRequestDto userSignupRequest) {
         UserAppResponseDto userAppResponseDto = userService.signupAdmin(userSignupRequest.toAppDto());
         return ResponseEntity.ok(UserResponseDto.from(userAppResponseDto));
     }
 
+    @Operation(summary = "관리자 권한 부여", description = "{username}회원의 권한 관리자로 변경", tags = {"사용자 관리 API"})
     @PatchMapping("/admin/users/{username}/roles")
-    public ResponseEntity<UserResponseDto> grantAdmin(@PathVariable("username") String username){
+    public ResponseEntity<UserResponseDto> grantAdmin(
+            @Parameter(description = "관리자 권한 부여받을 유저네임")
+            @PathVariable("username") String username){
         UserAppResponseDto userAppResponseDto = userService.grantAdmin(username);
         return ResponseEntity.ok(UserResponseDto.from(userAppResponseDto));
     }
 
+    @Operation(summary = "모든 유저 정보 조회", description = "모든 유저의 이름, 권한 조회", tags = {"사용자 관리 API"})
     @GetMapping("/users")
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         List<UserAppResponseDto> all = userService.findAll();
