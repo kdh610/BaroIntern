f50d186b4a6f882aaef6e3201a09c9925e96300a
kdh610 <k_dh610@naver.com>
2025-05-15 02:02:35 +0900

    fix: db import 삭제

--- src/main/java/com/sparta/barointern/domain/entity/User.java
+++ src/main/java/com/sparta/barointern/domain/entity/User.java
@@ -1,17 +1,19 @@
 package com.sparta.barointern.domain.entity;
 
 import com.sparta.barointern.domain.enums.UserRole;
-
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 public class User {
+
+
 
     private String username;
     private String nickname;
@@ -20,19 +22,22 @@ public class User {
     private List<Role> roles= new ArrayList<>(List.of(Role.create(UserRole.USER)));
 
     public void grantAdmin() {
-        if (this.roles == null) {
+        if (this.roles == null) { // 혹시 roles가 초기화 안됐을 수도 있으니 방어 코드
             this.roles = new ArrayList<>();
         }
+        // 이미 ADMIN 역할이 있는지 확인하고 없으면 추가
         if (!this.roles.contains(UserRole.ADMIN)) {
             this.roles.set(0,Role.create(UserRole.ADMIN));
         }
     }
+
 
     public static User create(String username, String password, String nickname) {
         return User.builder()
                 .username(username)
                 .nickname(nickname)
                 .password(password)
+//                .roles(roles)
                 .build();
     }
 
